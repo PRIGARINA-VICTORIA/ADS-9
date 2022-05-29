54c41942f559d02767063310ee66292d3623fa06
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-05-29 18:43:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,27 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+BST<std::string> tree;
+std::ifstream file("C:\\Users\\HUAWEI\\Downloads\\war_peace.txt");
+if (!file) {
+    std::cout << "File error!" << std::endl;
+    throw std::string("Can not open file!");
 }
+std::string word = "";
+while (!file.eof()) {
+    char ch = file.get();
+    int code = ch;
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+        word += tolower(code);
+    }
+    else {
+        if (word != "") {
+            tree.Add(word);
+            word = "";
+        }
+    }
+}
+file.close();
+return 0;
+}
+
