09764b53af9b3348cca3c2aa90db71d09ff61eba
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-05-29 20:27:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,27 +7,28 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
-BST<std::string> tree;
-std::ifstream file(filename);
-if (!file) {
-    std::cout << "File error!" << std::endl;
-    throw std::string("Can not open file!");
-}
-std::string word = "";
-while (!file.eof()) {
-    char ch = file.get();
-    int code = ch;
-    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
-        word += tolower(code);
-    } else {
-        if (word != "") {
-            tree.Add(word);
-            word = "";
+    // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        throw std::string("Can not open file!");
+    }
+    std::string word = "";
+    while (!file.eof()) {
+        char ch = file.get();
+        int code = ch;
+        if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+            word += tolower(code);
+        } else {
+            if (word != "") {
+                tree.Add(word);
+                word = "";
+            }
         }
     }
+    file.close();
+    return tree;
 }
-file.close();
-return tree;
-}
+
 
